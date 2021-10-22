402a914de27cbce0b3e90053af902fc8cbc3d703
Cole Killian <g.cole.killian@gmail.com>
2021-10-22 09:28:50 -0400

    feat: don't update all users on initial snapshot

--- src/index.ts
+++ src/index.ts
@@ -51,28 +51,35 @@ export class LunarAssistant {
       // cronjob to update discord roles once a day
       cron.schedule("0 0 * * *", () => this.updateAllDiscordUserRoles);
 
+      // only start triggering updates after the initial snapshot
+      let initialSnapshot = true;
+
       // update discord roles whenever a user document changes
       this.db.collection("users").onSnapshot((querySnapshot) => {
-        const changedDocs = querySnapshot.docChanges();
-        console.log("Docs changed: " + changedDocs.map((doc) => doc.doc.id));
-        changedDocs.reduce(
-          (p, changedDoc) =>
-            p
-              .then(() =>
-                this.updateDiscordRolesForUser(changedDoc.doc.id).catch(
-                  // ignore errors
-                  (error) => {}
-                )
-              )
-              .then(
-                // delay for one second between processing each user
-                () =>
-                  new Promise((resolve) =>
-                    setTimeout(() => resolve(null), 2000)
+        if (!initialSnapshot) {
+          const changedDocs = querySnapshot.docChanges();
+          console.log("Docs changed: " + changedDocs.map((doc) => doc.doc.id));
+          changedDocs.reduce(
+            (p, changedDoc) =>
+              p
+                .then(() =>
+                  this.updateDiscordRolesForUser(changedDoc.doc.id).catch(
+                    // ignore errors
+                    (error) => {}
                   )
-              ),
-          new Promise((resolve) => resolve(null))
-        );
+                )
+                .then(
+                  // delay for one second between processing each user
+                  () =>
+                    new Promise((resolve) =>
+                      setTimeout(() => resolve(null), 1000)
+                    )
+                ),
+            new Promise((resolve) => resolve(null))
+          );
+        } else {
+          initialSnapshot = false;
+        }
       });
 
       // listen to nft transfer events
