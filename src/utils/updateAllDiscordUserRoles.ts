4e2512be5296d5ee457ec1e5484b7b50979dd770
Cole Killian <g.cole.killian@gmail.com>
2021-11-22 16:20:59 -0500

    fix: handle cronjob errors

--- src/utils/updateAllDiscordUserRoles.ts
+++ src/utils/updateAllDiscordUserRoles.ts
@@ -1,4 +1,5 @@
 import { LunarAssistant } from "..";
+import { User } from "../shared/firestoreTypes";
 
 export async function updateAllDiscordUserRoles(this: LunarAssistant) {
   // read all users from firestore
@@ -17,21 +18,31 @@ export async function updateAllDiscordUserRoles(this: LunarAssistant) {
   const usersSnapshot = await this.db.collection("users").get();
   const guildConfigsSnapshot = await this.db.collection("guildConfigs").get();
 
+  const numUsers = usersSnapshot.docs.length;
+
   await usersSnapshot.docs.reduce(
-    (p, userDoc) =>
+    (p, userDoc, index) =>
       p
-        .then(() =>
+        .then(() => {
+          console.log(`Cronjob status: ${index} / ${numUsers}`);
           // update the user's discord roles
-          this.coldUpdateDiscordRolesForUser(
+          return this.coldUpdateDiscordRolesForUser(
             userDoc.id,
             userDoc,
             guildConfigsSnapshot
-          )
-        )
+          ).catch((error) => {
+            console.log(
+              `Failed to update roles for ${(userDoc.data() as User).wallet}`
+            );
+            console.error(error);
+          });
+        })
         .then(
           // delay for one second between processing each user
-          () => new Promise((resolve) => setTimeout(() => resolve(null), 1000))
+          () => new Promise((resolve) => setTimeout(() => resolve(null), 1500))
         ),
     new Promise((resolve, reject) => resolve(null))
   );
+
+  console.log("Finished cronjob!");
 }
