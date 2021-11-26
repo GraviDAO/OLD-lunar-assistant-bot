ead54a29c661a815c63469ce45215e3c445f5abc
Cole Killian <g.cole.killian@gmail.com>
2021-11-26 14:40:07 -0500

    feat: made deleteUserByWallet a cli

--- scripts/deleteUserByWallet.ts
+++ scripts/deleteUserByWallet.ts
@@ -1,3 +1,4 @@
+import commander from "commander";
 import admin, { ServiceAccount } from "firebase-admin";
 import { FIREBASE_ADMIN_SERVICE_ACCOUNT } from "../config.json";
 
@@ -14,15 +15,38 @@ if (!admin.apps.length) {
 }
 const db = admin.firestore();
 
-const coleID = "619663424812613662";
-const discordID = "418134977012563989";
-const walletAddress = "terra1j5g8xf526dahmg8eueegcnjetckldfcpu3tlq3";
-db.collection("users")
-  .where("wallet", "==", walletAddress)
-  .get()
-  .then((snapshot) => {
-    console.log(snapshot.docs.length);
-    snapshot.docs.forEach((doc) => {
-      db.collection("users").doc(doc.id).delete();
-    });
+const program = new commander.Command();
+
+program.requiredOption(
+  "-w, --wallet-address <walletAddress>",
+  "wallet address to unlink"
+);
+
+program.parse();
+
+const options = program.opts();
+
+const unlinkWallet = async (walletAddress: string) => {
+  const userSnapshot = await db
+    .collection("users")
+    .where("wallet", "==", walletAddress)
+    .get();
+
+  console.log(`Number of linked users: ${userSnapshot.docs.length}`);
+
+  if (userSnapshot.docs.length != 1) {
+    console.log("Error, number of linked users is not equal to 1");
+    return;
+  }
+
+  userSnapshot.docs.forEach((doc) => {
+    db.collection("users")
+      .doc(doc.id)
+      .delete()
+      .then(() => {
+        console.log(`User unlinked: ${doc.id}`);
+      });
   });
+};
+
+unlinkWallet(options.walletAddress);
