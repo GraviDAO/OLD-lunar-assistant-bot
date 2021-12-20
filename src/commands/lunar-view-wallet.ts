fba347a6f4a6ae203d06a8238f3cc6fcd0eaf5b3
Cole Killian <g.cole.killian@gmail.com>
2021-12-20 03:09:42 -0500

    fix: lunar-view-wallet update copy

--- src/commands/lunar-view-wallet.ts
+++ src/commands/lunar-view-wallet.ts
@@ -2,6 +2,7 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
 import { LunarAssistant } from "..";
 import { passportApi } from "../services/passport";
+import { Users } from "../shared/firestoreTypes";
 
 export default {
   data: new SlashCommandBuilder()
@@ -16,16 +17,27 @@ export default {
       interaction.user.id
     );
 
-    if (walletAddresses.length > 0) {
+    const users = (
+      await lunarAssistant.db.collection("root").doc("users").get()
+    ).data() as Users;
+
+    if (
+      users.discordIds.includes(interaction.user.id) &&
+      walletAddresses.length > 0
+    ) {
       await interaction.reply({
         content: `Your registered wallets: ${walletAddresses.join(",")}`,
         ephemeral: true,
       });
-    } else {
-      await interaction.reply({
+    } else if (walletAddresses.length > 0) {
+      await interaction.editReply({
         content:
-          "You haven't linked any wallets yet. Link a wallet with /lunar-link",
-        ephemeral: true,
+          "Cannot display your wallet because you haven't activated Lunar Assistant for your Discorda account yet. Please activate Lunar Assistant with /lunar-activate.",
+      });
+    } else {
+      await interaction.editReply({
+        content:
+          "Cannot display your wallet because you haven't linked your wallet with Galactic Passport or activated Lunar Assistant for your Discord account. Please go to https://galacticpassport.com, log in with your Discord account, link your relevant wallets, and then try running `/lunar-activate`.`,.",
       });
     }
   },
