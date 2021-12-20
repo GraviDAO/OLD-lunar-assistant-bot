f14dba23a9cb02586193010691e0ffcfbd802bf7
Cole Killian <g.cole.killian@gmail.com>
2021-12-20 03:25:41 -0500

    feat: add script for migrating to passport

---
+++ scripts/migrateToPassport.ts
@@ -0,0 +1,39 @@
+import admin, { ServiceAccount } from "firebase-admin";
+import { FIREBASE_ADMIN_SERVICE_ACCOUNT } from "../config.json";
+import { passportApi } from "../src/services/passport";
+import { User } from "../src/shared/firestoreTypes";
+
+// loop through all users
+
+if (!admin.apps.length) {
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(
+        FIREBASE_ADMIN_SERVICE_ACCOUNT as ServiceAccount
+      ),
+    });
+  } catch (error) {
+    console.error("Firebase admin initialization error", error);
+  }
+}
+const db = admin.firestore();
+
+const migrateToPassport = async () => {
+  const usersSnapshot = await db.collection("users").get();
+  const discordIds: string[] = [];
+
+  // For each doc, link on galactic passport and
+  await usersSnapshot.docs.reduce(
+    (p, doc) =>
+      p.then(() => {
+        const wallet = (doc.data() as User).wallet;
+        discordIds.push(doc.id);
+        console.log(`Linking ${wallet} to ${doc.id}`);
+        return passportApi.linkAddressToDiscordId(wallet, doc.id);
+      }),
+    new Promise((resolve) => resolve(null))
+  );
+
+  // Update the users doc
+  await db.collection("root").doc("users").set({ discordIds });
+};
