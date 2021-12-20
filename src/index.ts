5b12081fbd37dc5c17464bf04d14d600218b2cd0
Cole Killian <g.cole.killian@gmail.com>
2021-12-20 03:09:53 -0500

    fix: don't perform the webhook updates

--- src/index.ts
+++ src/index.ts
@@ -71,33 +71,33 @@ export class LunarAssistant {
       // only start triggering updates after the initial snapshot
       let initialSnapshot = true;
 
-      // update discord roles whenever a user document changes
-      this.db.collection("users").onSnapshot((querySnapshot) => {
-        if (!initialSnapshot) {
-          const changedDocs = querySnapshot.docChanges();
-          console.log("Docs changed: " + changedDocs.map((doc) => doc.doc.id));
-          changedDocs.reduce(
-            (p, changedDoc) =>
-              p
-                .then(() =>
-                  this.updateDiscordRolesForUser(changedDoc.doc.id).catch(
-                    // ignore errors
-                    (error) => {}
-                  )
-                )
-                .then(
-                  // delay for one second between processing each user
-                  () =>
-                    new Promise((resolve) =>
-                      setTimeout(() => resolve(null), 1000)
-                    )
-                ),
-            new Promise((resolve) => resolve(null))
-          );
-        } else {
-          initialSnapshot = false;
-        }
-      });
+      // // update discord roles whenever a user document changes
+      // this.db.collection("users").onSnapshot((querySnapshot) => {
+      //   if (!initialSnapshot) {
+      //     const changedDocs = querySnapshot.docChanges();
+      //     console.log("Docs changed: " + changedDocs.map((doc) => doc.doc.id));
+      //     changedDocs.reduce(
+      //       (p, changedDoc) =>
+      //         p
+      //           .then(() =>
+      //             this.updateDiscordRolesForUser(changedDoc.doc.id).catch(
+      //               // ignore errors
+      //               (error) => {}
+      //             )
+      //           )
+      //           .then(
+      //             // delay for one second between processing each user
+      //             () =>
+      //               new Promise((resolve) =>
+      //                 setTimeout(() => resolve(null), 1000)
+      //               )
+      //           ),
+      //       new Promise((resolve) => resolve(null))
+      //     );
+      //   } else {
+      //     initialSnapshot = false;
+      //   }
+      // });
 
       // listen to nft transfer events
       this.connectObserver();
