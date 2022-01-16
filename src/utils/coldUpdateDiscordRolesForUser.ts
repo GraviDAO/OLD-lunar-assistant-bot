239a68914bf460996d2d5427a0c2dbb618f8a72b
Cole Killian <g.cole.killian@gmail.com>
2022-01-15 18:27:24 -0800

    fix: handle multiple rules with same role

--- src/utils/coldUpdateDiscordRolesForUser.ts
+++ src/utils/coldUpdateDiscordRolesForUser.ts
@@ -86,51 +86,40 @@ export async function coldUpdateDiscordRolesForUser(
         numMatchingTokens = numTokens;
       }
 
+      // How to deal with multiple roles that have the same name?
+
       if (numMatchingTokens >= rule.quantity) {
         // the user matches the role rules, update accordingly
 
-        try {
-          // update activeRoles
-          if (activeRoles[guild.name]) {
-            activeRoles[guild.name].push(newRole.name);
-          } else {
-            activeRoles[guild.name] = [newRole.name];
-          }
+        // update activeRoles
+        if (activeRoles[guild.name]) {
+          activeRoles[guild.name].push(newRole.name);
+        } else {
+          activeRoles[guild.name] = [newRole.name];
+        }
 
-          if (member) {
-            // add the role to the member
-            await member.roles.add(newRole);
-          }
-        } catch (e) {
-          console.error(
-            "Couldn't add role, probably because of role hierarchy.",
-            guild.name,
-            newRole.name
+        // if removedRoles includes the role, delete it
+        if (removedRoles[guild.name].includes(newRole.name)) {
+          removedRoles[guild.name].splice(
+            removedRoles[guild.name].indexOf(newRole.name)
           );
         }
       } else if (
+        // make sure member exists
         member &&
-        member.roles.cache.some((role) => role.name === newRole.name)
+        // make sure member has role
+        member.roles.cache.some((role) => role.name === newRole.name) &&
+        // make sure member wasn't granted the role
+        !activeRoles[guild.name].includes(newRole.name)
       ) {
         // the user doesn't match the role rules but has the role anyways
         // update accordingly
 
-        try {
-          // update removedRoles
-          if (removedRoles[guild.name]) {
-            removedRoles[guild.name].push(newRole.name);
-          } else {
-            removedRoles[guild.name] = [newRole.name];
-          }
-
-          // remove the role from the member
-          await member.roles.remove(newRole);
-        } catch (e) {
-          console.error(
-            "Couldn't remove role, probably because of role hierarchy.",
-            guild.name,
-            newRole.name
-          );
+        // update removedRoles
+        if (removedRoles[guild.name]) {
+          removedRoles[guild.name].push(newRole.name);
+        } else {
+          removedRoles[guild.name] = [newRole.name];
         }
       }
     };
@@ -157,7 +146,56 @@ export async function coldUpdateDiscordRolesForUser(
       try {
         const member = await guild.members.fetch(userID);
 
+        // if not a member then skip
+        if (!member) return;
+
         coldUpdateDiscordRolesForUserInGuild(guild, member, guildConfigDoc);
+
+        // add the relevant roles
+        await Promise.all(
+          activeRoles[guild.name].map(async (roleName) => {
+            // get the discord role from the discord client
+            const newRole = guild.roles.cache.find(
+              (role) => role.name == roleName
+            );
+
+            if (!newRole) return;
+
+            try {
+              // add the role to the member
+              await member.roles.add(newRole);
+            } catch (e) {
+              console.error(
+                "Couldn't add role, probably because of role hierarchy.",
+                guild.name,
+                newRole.name
+              );
+            }
+          })
+        );
+
+        // remove the relevant roles
+        await Promise.all(
+          removedRoles[guild.name].map(async (roleName) => {
+            // get the discord role from the discord client
+            const newRole = guild.roles.cache.find(
+              (role) => role.name == roleName
+            );
+
+            if (!newRole) return;
+
+            try {
+              // remove the role from the member
+              await member.roles.remove(newRole);
+            } catch (e) {
+              console.error(
+                "Couldn't remove role, probably because of role hierarchy.",
+                guild.name,
+                newRole.name
+              );
+            }
+          })
+        );
       } catch (e) {
         // member doesn't exist in guild
       }
