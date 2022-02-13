cec369dc158a8d1a5c90056495aa1c9cc2cc8a63
Cole Killian <g.cole.killian@gmail.com>
2022-02-12 20:06:55 -0800

    feat: better error logging

--- src/utils/terraHelpers.ts
+++ src/utils/terraHelpers.ts
@@ -47,7 +47,7 @@ export const getWalletTokensOfOwner = async (
   } catch (e: any) {
     // console.error(e);
     console.error(
-      `Couldn't query tokens for ${contractAddress}. Status code ${e?.response?.status}`
+      `Couldn't query tokens for ${contractAddress}. Status code ${e?.response?.status}. Data: ${e?.response?.data}`
     );
   }
   return res;
