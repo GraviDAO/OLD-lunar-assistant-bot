ee24e9bedb104fbcff5495fdbb42d6cb81ef565f
DRITConsulting <100054192+DRITConsulting@users.noreply.github.com>
2022-03-12 11:32:54 -0500

    add new test to verify return value is false when an error is caught

--- tests/utils/getCustomAPIWalletAllowed.test.ts
+++ tests/utils/getCustomAPIWalletAllowed.test.ts
@@ -12,4 +12,11 @@ test('get API Response: Not Allowed', async () => {
     })
 });
 
+test('get API Response: error -> Not Allowed', async () => {
+    //URL is wrong HTTP error 404 will be returned
+    getCustomAPIWalletAllowed("https://stations.levana.finance/api/factions/freeans?wallet=$(wallet)", "terra18nxhzmg6733r93wes7d75udfmdhr9uze0ka4rd").then(data => {
+        expect(data).toEqual(false);
+    })
+});
+
 //todo: add tests to validate inputs
