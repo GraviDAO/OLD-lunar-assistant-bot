74a7194f5899e889b485f26bff205ae64933b810
David <david.richard@dritconsulting.ca>
2022-05-10 06:22:17 -0400

    Switch to global commands

--- src/utils/registerCommands.ts
+++ src/utils/registerCommands.ts
@@ -6,21 +6,7 @@ import { clientId, token } from "../../config.json";
 import { commandFiles } from "./commandFiles";
 import { contextMenuFiles } from "./contextMenuFiles";
 
-export const registerCommands = async (guild: Guild) => {
-  // Check if an existing lunar commander role exists in the server
-  const existingLunarCommanderRole = guild.roles.cache.find(
-    (role) => role.name === "Lunar Commander"
-  );
-
-  // If no such role exists, create it
-  const lunarCommanderRole = existingLunarCommanderRole
-    ? existingLunarCommanderRole
-    : await guild.roles.create({
-        name: "Lunar Commander",
-        color: "BLUE",
-        reason: "Managing the lunar assistant.",
-      });
-
+export const registerCommands = async () => {
   // get the list of new commands
   const newCommands = commandFiles.map((file) => {
     const commandFilePath = path.resolve(__dirname, `../commands/${file}`);
@@ -29,38 +15,20 @@ export const registerCommands = async (guild: Guild) => {
   });
 
   const newContextMenus = contextMenuFiles.map((file) => {
-    const contextMenuFilePath = path.resolve(__dirname, `../contextMenus/${file}`);
+    const contextMenuFilePath = path.resolve(
+      __dirname,
+      `../contextMenus/${file}`
+    );
     const contextMenu = require(contextMenuFilePath).default;
     return contextMenu.data.toJSON();
-  });  
+  });
 
   // register the commands
   const rest = new REST({ version: "9" }).setToken(token);
 
-  await rest.put(Routes.applicationGuildCommands(clientId, guild.id) as any, {
+  await rest.put(Routes.applicationCommands(clientId) as any, {
     body: newCommands.concat(newContextMenus),
   });
 
-  console.log(`Successfully registered application commands for ${guild.name}`);
-
-  // now update the permissions to allow for lunar commanders to configure the lunar assistant
-  const permissions = [
-    {
-      id: lunarCommanderRole.id,
-      type: "ROLE" as const,
-      permission: true,
-    },
-  ];
-
-  const registeredCommands = await guild.commands.fetch();
-
-  // get the configured command
-  const configureCommand = registeredCommands.find((command) => {
-    return command.name == "lunar-configure";
-  });
-
-  // add the lunar command permission
-  // As of 2022-04-28 after discord update the bot can no longer give lunar-configure command permission to the lunar commander role
-    //await configureCommand!.permissions.add({ permissions });
-
+  console.log(`Successfully registered application commands`);
 };
